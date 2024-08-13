efd11f640a3e6ad33be750addfcf138ef75b6983
Hoto Ras <hotoras03@gmail.com>
2024-08-14 06:43:18 +0900

    Create 1723585019082-noteCreatedAtMissing.js
    note.createdAt이 psql 쿼리 중 테이블 상에 없어서 오류 발생, 조치를 위해 마이그레이션 추가

---
+++ packages/backend/migration/1723585019082-noteCreatedAtMissing.js
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+// 1723585019082-noteCreatedAtMissing.js
+export class NoteCreatedAtMissing1723585019082 {
+    name = 'NoteCreatedAtMissing1723585019082'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "note" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()`);
+    }
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "note" DROP COLUMN "createdAt"`);
+    }
+
+}
