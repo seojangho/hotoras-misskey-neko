be84a11facfa869c8cb3ad4f09554fe29b7d4511
HotoRas <hotoras03@gmail.com>
2024-09-14 12:34:53 +0900

    Revert "migration: set IF NOT EXISTS to column creation DDL"
    This reverts commit 69ed45d25bd72b9ae7690fe88a5320b09c672600.

--- packages/backend/migration/1720853122058-RevRevertNoteEdit.js
+++ packages/backend/migration/1720853122058-RevRevertNoteEdit.js
@@ -7,9 +7,9 @@ export class RevRevertNoteEdit1720853122058 {
     name = 'RevRevertNoteEdit1720853122058'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "note" ADD IF NOT EXISTS "updatedAt" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "note" ADD IF NOT EXISTS "updatedAtHistory" TIMESTAMP WITH TIME ZONE ARRAY`);
-        await queryRunner.query(`ALTER TABLE "note" ADD IF NOT EXISTS "noteEditHistory" character varying array`)
+        await queryRunner.query(`ALTER TABLE "note" ADD "updatedAt" TIMESTAMP WITH TIME ZONE`);
+        await queryRunner.query(`ALTER TABLE "note" ADD "updatedAtHistory" TIMESTAMP WITH TIME ZONE ARRAY`);
+        await queryRunner.query(`ALTER TABLE "note" ADD "noteEditHistory" character varying array`)
     }
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "note" DROP COLUMN "updatedAt"`);
