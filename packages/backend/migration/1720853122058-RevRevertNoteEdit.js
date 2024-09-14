5d636478b9bbc20850283ea71597dae2beff2e9b
HotoRas <hotoras03@gmail.com>
2024-09-14 12:58:43 +0900

    fix: 마이그레이션 코드가 해당 테이블이 있는지 확인하도록
    안하면 터짐

--- packages/backend/migration/1720853122058-RevRevertNoteEdit.js
+++ packages/backend/migration/1720853122058-RevRevertNoteEdit.js
@@ -7,14 +7,14 @@ export class RevRevertNoteEdit1720853122058 {
     name = 'RevRevertNoteEdit1720853122058'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "note" ADD "updatedAt" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "note" ADD "updatedAtHistory" TIMESTAMP WITH TIME ZONE ARRAY`);
-        await queryRunner.query(`ALTER TABLE "note" ADD "noteEditHistory" character varying array`)
+        await queryRunner.query(`ALTER TABLE "note" IF NOT EXIST "updatedAt" ADD "updatedAt" TIMESTAMP WITH TIME ZONE`);
+        await queryRunner.query(`ALTER TABLE "note" IF NOT EXIST "updatedAtHistory" ADD "updatedAtHistory" TIMESTAMP WITH TIME ZONE ARRAY`);
+        await queryRunner.query(`ALTER TABLE "note" IF NOT EXIST "noteEditHistory" ADD "noteEditHistory" character varying array`)
     }
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "note" DROP COLUMN "updatedAt"`);
-        await queryRunner.query(`ALTER TABLE "note" DROP "updatedAtHistory" TIMESTAMP WITH TIME ZONE ARRAY`);
-        await queryRunner.query(`ALTER TABLE "note" DROP "noteEditHistory"`)
+        await queryRunner.query(`ALTER TABLE "note" IF EXISTS "updatedAt" DROP COLUMN "updatedAt"`);
+        await queryRunner.query(`ALTER TABLE "note" IF EXISTS "updatedAt" DROP "updatedAtHistory"`);
+        await queryRunner.query(`ALTER TABLE "note" IF EXISTS "updatedAt" DROP "noteEditHistory"`)
     }
 
 }
